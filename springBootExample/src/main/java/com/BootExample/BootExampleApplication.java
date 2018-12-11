01ea96a11f54fb3292738485169ee927fba9f154
KimJiYong <KimJiYong@KimJiYong-PC>
2018-12-11 16:44:09 +0900

    ㄴㅇㄻㅇㄹ

--- springBootExample/src/main/java/com/BootExample/BootExampleApplication.java
+++ springBootExample/src/main/java/com/BootExample/BootExampleApplication.java
@@ -2,11 +2,17 @@ package com.BootExample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.BootExample")
 public class BootExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootExampleApplication.class, args);
+		
+		
+        
+        
 	}
 }
