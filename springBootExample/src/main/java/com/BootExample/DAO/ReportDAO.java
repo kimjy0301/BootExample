2dcb4fbdc8ad7fd4fafdcf1b017da9607031e478
KimJiYong <KimJiYong@KimJiYong-PC>
2018-12-13 15:43:47 +0900

    a

--- springBootExample/src/main/java/com/BootExample/DAO/ReportDAO.java
+++ springBootExample/src/main/java/com/BootExample/DAO/ReportDAO.java
@@ -3,12 +3,16 @@ package com.BootExample.DAO;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.BootExample.Model.Report;
 
 public interface ReportDAO extends JpaRepository<Report, Integer>{	
 
+	
+	
+	@Query("SELECT t.patNo, t.patName, t.examTm, t.report FROM Report t where t.patNo = :patNo")	
 	List<Report> findByPatNo(@Param("patNo") String patNo);
 
 	List<Report> findByPatName(@Param("patName") String patName);
--- springBootExample/src/main/java/com/BootExample/Model/Report.java
+++ springBootExample/src/main/java/com/BootExample/Model/Report.java
@@ -2,10 +2,7 @@ package com.BootExample.Model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -13,12 +10,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "REPORTWL")
-@SequenceGenerator(name = "REPORTWL_SEQ_GENERATOR", sequenceName = "REPORTWL_SEQ", initialValue = 1, allocationSize = 1)
 public class Report {
-
-	@Id
-	@Column(name = "ROWID")
-	private String rowId;
 
 	@Column(name = "PATIENT_ID")
 	private String patNo;
@@ -31,4 +23,8 @@ public class Report {
 
 	@Column(name = "REPORT_TEXT")
 	private String report;
+
+	@Id
+	@Column(name = "ROWID")
+	private String rowId;
 }
